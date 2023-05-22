ec319af30019a7ab64e67ea60cd57f0f682c20f7
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-22 17:26:13 +0200

    ✨ add country code component to display details

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,9 +1,33 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
+
+const GET_COUNTRY = gql`
+  query getcountry($code: ID!) {
+    country(code: $code) {
+      name
+      emoji
+      currency
+      capital
+    }
+  }
+`;
 
 function Country() {
+  let { countryCode } = useParams();
+  const { loading, error, data } = useQuery(GET_COUNTRY, {
+    variables: { code: countryCode },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
     <>
-      <h1>Country detail</h1>
+      <h1>{data.country.name}</h1>
+      <p>{data.country.emoji}</p>
+      <p>Currency:{data.country.currency}</p>
+      <p>Capital:{data.country.capital}</p>
     </>
   );
 }
