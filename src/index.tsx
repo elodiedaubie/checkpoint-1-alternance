3dcf68539a5ca5ae9a2119aa1bf4ad1f4ab1699d
Elodie Daubie <elodie.daubie@gmail.com>
2023-05-22 14:44:21 +0200

    🔧 initialize appollo client and first test

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://countries.nausicaa.wilders.dev/",
+  cache: new InMemoryCache(),
+});
+
+// first query to test apollo client
+client
+  .query({
+    query: gql`
+      query GetContinents {
+        continents {
+          name
+          countries {
+            code
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
